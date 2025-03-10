89af86668b3c7c6c381ce462e6eeee7dab63c177
João Marcos <111158327+Joaozinho723@users.noreply.github.com>
2025-03-10 12:12:55 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+document.getElementById("btn").addEventListener("click", function() {
+    document.getElementById("menu").classList.toggle("hidden");
+});
+
+document.addEventListener('DOMContentLoaded', function(){
+    const aumentaFonteBotao = document.getElementById('diminuir-fonte');
+    const diminuiFonteBotao = document.getElementById('aumentar-fonte');
+
+    let tamanhoAtualFonte = 1;
+    aumentaFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+    })
+
+    diminuiFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+    })
+
+})
+
